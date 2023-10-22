468551534cdb8768cef0c9ee0a28fcf1e5e9a934
BD3849 <bsdinnan@icloud.com>
2023-10-21 23:33:53 -0400

    general changes, setting up structure of application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutPage from './pages/about_page/AboutPage';
+import ContactPage from './pages/contact_page/ContactPage';
+import PortfolioPage from './pages/portfolio_page/PortfolioPage';
+import ResumePage from './pages/resume_page/ResumePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: '/resume',
+        element: <ResumePage />,
+      },
+      {
+        path: '/portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
