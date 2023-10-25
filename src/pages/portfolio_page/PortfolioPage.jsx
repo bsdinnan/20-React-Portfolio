b42b433c1395245cedc7bc8991a01e45e16954bc
BD3849 <bsdinnan@icloud.com>
2023-10-25 01:20:15 -0400

    final for now

--- src/pages/portfolio_page/PortfolioPage.jsx
+++ src/pages/portfolio_page/PortfolioPage.jsx
@@ -1,10 +1,11 @@
 import Project from '../../components/project/Project';
 import projectsData from '../../../public/projects';
+import "./portfolioPage.css"
 
 export default function PortfolioPage() {
     return (
         <div className="d-flex flex-column align-items-center container flex fill">
-            <p className="text-center p-3 m-2 text-bg-dark shadow-lg col-sm-12 col-lg-10 rounded">
+            <p id='Par' className="text-center p-3 m-2 text-bg-dark shadow-lg col-sm-12 col-lg-10 rounded">
                 Here are some projects I have worked on in my bootcamp, as well as any I've created on my own.
             </p>
             <div className="container d-flex flex-wrap align-items-evenly justify-content-evenly">
